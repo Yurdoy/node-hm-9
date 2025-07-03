81da45e5ffed730c69b581212f988dd3ffc2b996
Yurdoy <yurdoy@gmail.com>
2025-07-03 09:47:55 +0200

    Model 'User' created.

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "Users",
+    timestamps: false,
+  }
+);
+
+export default User;
