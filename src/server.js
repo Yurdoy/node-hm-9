ef5763831eb065437ca1cd1f9ac77d474c3b335d
Yurdoy <yurdoy@gmail.com>
2025-07-03 10:41:55 +0200

    Task 1 - 'POST /register' creation, new user registration and email unique check completed.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import sequelize from "./config/db.js";
+import bcrypt from "bcrypt";
+import User from "./models/User.js";
 
 const app = express();
 app.use(express.json());
@@ -8,6 +10,23 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, sequelize with Express");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(404).json({ message: "All fields are required" });
+    }
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email has already registered" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, async () => {
