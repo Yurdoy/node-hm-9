02b7b463eba08d37c4a48a9cb95313de00dde0b8
Yurdoy <yurdoy@gmail.com>
2025-07-03 14:46:16 +0200

    POST /change-email created

--- src/server.js
+++ src/server.js
@@ -95,6 +95,44 @@ app.post("/change-password", async (req, res) => {
   }
 });
 
+app.post("/change-email", authMiddleWare, async (req, res) => {
+  const { newEmail, currentPassword } = req.body;
+  const userId = req.user.id;
+  try {
+    if (!newEmail || !currentPassword) {
+      return res
+        .status(400)
+        .json({ message: "New email and current password are required" });
+    }
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    const existingUser = await User.findOne({ where: { email: newEmail } });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email already registered" });
+    }
+    user.email = newEmail;
+    await user.save();
+    return res
+      .status(500)
+      .json({
+        message: "Email changed successfully",
+        user: { id: user.id, email: user.email },
+      });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 app.post("/delete-account", authMiddleWare, async (req, res) => {
   const { password } = req.body;
   const userId = req.user.id;
